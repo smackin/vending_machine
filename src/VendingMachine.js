151dfad7e33b83000a08ee268fb92d0a9f700a56
Stephanie <smmackin12@gmail.com>
2022-09-28 13:20:39 -0400

    styling is weird, but functional

--- src/VendingMachine.js
+++ src/VendingMachine.js
@@ -1,16 +1,32 @@
 import React from 'react';
-import "./App.css"
-import { BrowserRouter, Route } from 'react-router-dom';
+import './App.css';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 import Reeses from './Reeses';
 import Chips from './Chips';
 import Soda from './Soda';
 
 function VendingMachine() {
 	return (
-		<div>
-			<h2> I AM THE MIGHTY VENDING MACHINE</h2>
-            <p> Make your selection wisely..</p>
-            <img src="https://images.unsplash.com/photo-1625650484478-113df4bfc370?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8dmVuZGluZyUyMG1hY2hpbmV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"/>
+		<div
+			className="VendingMachine"
+			style={{
+				backgroundImage:
+					'url("https://images.unsplash.com/photo-1625650484478-113df4bfc370?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8dmVuZGluZyUyMG1hY2hpbmV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60")'
+			}}
+		>
+
+			<h1>
+				<Link to="/reeses">PB CUPS</Link>
+			</h1>
+			<h1>
+				<Link to="/chips">Ruffles Chips</Link>
+			</h1>
+			<h1>
+				<Link to="/soda">Ice Cold Soda</Link>
+			</h1>
+			<h1>
+				<Link to="/popcorn">Freshly Popped Popcorn</Link>
+			</h1>
 		</div>
 	);
 }
